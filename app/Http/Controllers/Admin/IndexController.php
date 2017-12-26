1796dbc9c97ad63e3a4b2fc04bcca9d7bff31d01
jerrygaoyang <917616767@qq.com>
2017-12-26 18:21:13 +0800

    update

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -9,6 +9,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+//        return view('admin.index');
+        return redirect('admin/iot/product');
     }
 }
