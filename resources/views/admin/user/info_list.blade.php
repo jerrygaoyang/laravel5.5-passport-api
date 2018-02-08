32b867bb073ac0cb20b19ab86b8becbece9c22a7
jerrygaoyang <917616767@qq.com>
2018-02-08 16:02:12 +0800

    update

--- resources/views/admin/user/info_list.blade.php
+++ resources/views/admin/user/info_list.blade.php
@@ -7,11 +7,11 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="page-title-box">
-                    <h4 class="page-title">开门记录</h4>
+                    <h4 class="page-title">用户列表</h4>
                     <ol class="breadcrumb float-right">
                         <li class="breadcrumb-item"><a href="#">Minton</a></li>
-                        <li class="breadcrumb-item"><a href="#">房屋管理</a></li>
-                        <li class="breadcrumb-item active">开门记录</li>
+                        <li class="breadcrumb-item"><a href="#">用户管理</a></li>
+                        <li class="breadcrumb-item active">用户列表</li>
                     </ol>
                     <div class="clearfix"></div>
                 </div>
@@ -35,19 +35,21 @@
                         <table class="table table-bordered" id="datatable-editable">
                             <thead>
                             <tr>
-                                <th>房间</th>
                                 <th>用户</th>
                                 <th>手机</th>
-                                <td>开门时间</td>
+                                <th>性别</th>
+                                <th>房间</th>
+                                <td>租赁时间</td>
                                 <th>操作</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
-                                <td>上海-xxx-xxx-xxx 1001室</td>
                                 <td>高杨</td>
                                 <td>18888888888</td>
-                                <td>2018-01-03 19:23</td>
+                                <td>男</td>
+                                <td>上海-xxx-xxx-xxx 1001室</td>
+                                <td>2018-01-03 至 2010-01-03</td>
                                 <td>
                                     <a href="#"
                                        class="on-default edit-row my-handle"
@@ -61,10 +63,11 @@
                                 </td>
                             </tr>
                             <tr>
-                                <td>上海-xxx-xxx-xxx 1002室</td>
                                 <td>小明</td>
                                 <td>18888888888</td>
-                                <td>2018-01-02 16:23</td>
+                                <td>男</td>
+                                <td>上海-xxx-xxx-xxx 1002室</td>
+                                <td>2018-01-02 至 2019-01-02</td>
                                 <td>
                                     <a href="#"
                                        class="on-default edit-row my-handle"
@@ -78,10 +81,11 @@
                                 </td>
                             </tr>
                             <tr>
-                                <td>上海-xxx-xxx-xxx 1003室</td>
                                 <td>张三</td>
                                 <td>18888888888</td>
-                                <td>2018-01-01 20:21</td>
+                                <td>男</td>
+                                <td>上海-xxx-xxx-xxx 1003室</td>
+                                <td>2018-01-01 至 2019-01-01</td>
                                 <td>
                                     <a href="#"
                                        class="on-default edit-row my-handle"
@@ -95,27 +99,10 @@
                                 </td>
                             </tr>
                             <tr>
+                                <td>李四</td>
+                                <td>18888888888</td>
                                 <td>上海-xxx-xxx-xxx 1002室</td>
-                                <td>小明</td>
-                                <td>18888888888</td>
-                                <td>2018-01-01 19:15</td>
-                                <td>
-                                    <a href="#"
-                                       class="on-default edit-row my-handle"
-                                       data-toggle="tooltip" data-placement="top" data-original-title="编辑">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                    <a onclick="" class="on-default remove-row my-handle"
-                                       data-toggle="tooltip" data-placement="top" data-original-title="删除">
-                                        <i class="fa fa-trash-o"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>上海-xxx-xxx-xxx 1001室</td>
-                                <td>高杨</td>
-                                <td>18888888888</td>
-                                <td>2018-12-20 15:23</td>
+                                <td>2018-01-01 至 2019-01-01</td>
                                 <td>
                                     <a href="#"
                                        class="on-default edit-row my-handle"
